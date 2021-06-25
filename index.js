57c4f96a4bcde429e873c1745651cda42eb48173
Matheus Mahnke <BRHBS0696@ambev.com.br>
2021-06-25 16:58:25 -0300

    initial

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.use('/assets/css', express.static(__dirname + '/assets/css'));
+app.use('/assets/fonts', express.static(__dirname + '/assets/fonts'));
+app.use('/assets/images', express.static(__dirname + '/assets/images'));
+app.use('/assets/js', express.static(__dirname + '/assets/js'));
+
+app.use('/vendor/bootstrap/css', express.static(__dirname + '/vendor/bootstrap/css'));
+app.use('/vendor/bootstrap/js', express.static(__dirname + '/vendor/bootstrap/js'));
+app.use('/vendor/jquery', express.static(__dirname + '/vendor/jquery'));
+
+
+app.get('/health', function (req, res) {
+  res.send('Healthy!');
+});
+
+app.get('', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', function (req, res) {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/about', function (req, res) {
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/products', function (req, res) {
+  res.sendFile(__dirname + '/products.html');
+});
+
+app.listen(8000, function () {
+  console.log('Listening to Port 8000');
+});
